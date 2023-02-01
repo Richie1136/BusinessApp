b9dcfcc6de1abdaf243a0359ebfc46fbd6d768bf
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2023-02-01 10:00:44 -0500

    Building out the structure of the app

--- businessapp/src/App.js
+++ businessapp/src/App.js
@@ -1,10 +1,40 @@
+import styles from "./style";
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Business App</h2>
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        {/* <NavBar /> */}
+        NavBar
+      </div>
     </div>
-  );
-}
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        {/* <Hero /> */}
+        Hero
+      </div>
+    </div>
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        {/* <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <ClickToAction />
+        <Footer /> */}
+        Stats
+        Business
+        Billing
+        CardDeal
+        Testimonials
+        Clients
+        ClickToAction
+        Footer
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
