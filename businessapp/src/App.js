0503678df1d45827d8bfff90302673e97d177963
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2023-02-01 11:04:47 -0500

    Building out the navbar component

--- businessapp/src/App.js
+++ businessapp/src/App.js
@@ -11,7 +11,6 @@ const App = () => (
     <div className={`bg-primary ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
         <Hero />
-
       </div>
     </div>
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
--- businessapp/src/components/index.js
+++ businessapp/src/components/index.js
@@ -1,15 +1,12 @@
-import Billing from "./billing/Billing";
-import Business from "./business/Business";
-import Button from "./button/Button";
-import CardDeal from "./carddeal/CardDeal";
-import ClickToAction from "./clicktoaction/ClickToAction";
-import Clients from "./clients/Clients";
-import FeedbackCard from "./feedbackcard/FeedbackCard";
-import Footer from "./footer/Footer";
-import GetStarted from "./getstarted/GetStarted";
-import Hero from "./hero/Hero";
 import NavBar from "./navbar/NavBar";
+import Billing from "./billing/Billing";
+import CardDeal from "./carddeal/CardDeal";
+import Business from "./business/Business";
+import Clients from "./clients/Clients";
+import ClickToAction from "./clicktoaction/ClickToAction";
 import Stats from "./stats/Stats";
+import Footer from "./footer/Footer";
 import Testimonials from "./testimonials/Testimonials";
+import Hero from "./hero/Hero";
 
-export { Billing, Testimonials, Stats, NavBar, Hero, Business, GetStarted, Button, CardDeal, ClickToAction, Clients, FeedbackCard, Footer }
+export { Billing, Testimonials, Stats, NavBar, Hero, Business, CardDeal, ClickToAction, Clients, Footer }
