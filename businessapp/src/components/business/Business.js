54a991a2431ce75e78fb1c7a380e88723a344abe
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2023-02-02 09:57:16 -0500

    Mapping over the features and returning the feature data as a featurecard component and passing the different data as props to the featurescard component

--- businessapp/src/components/business/Business.js
+++ businessapp/src/components/business/Business.js
@@ -1,7 +1,31 @@
+import { features } from "../../constants"
+import styles, { layout } from "../../style"
+import Button from '../button/Button'
+
+const FeatureCard = ({ icon, title, content, id }) => (
+  <div className={`flex flex-row p-6 rounded-[20px] ${id !== features.length - 1 ? 'mb-6' : 0} feature-card`}>
+    <div>
+      <img src={icon} alt='icons' className="w-[50%] h-[50%] object-contain" />
+    </div>
+  </div>
+)
 
 const Business = () => {
   return (
-    <div>Business</div>
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>You do the business, <br className="sm:block hidden" /> we'll handle the money.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right credit card, you can improve your financial life by
+          building credit, earning rewards and saving money. But with hundreds
+          of credit cards on the market.</p>
+        <Button styles="mt-10" />
+      </div>
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map(({ id, icon, title, content }) => (
+          <FeatureCard key={id} icon={icon} title={title} content={content} />
+        ))}
+      </div>
+    </section>
   )
 }
 
