4603a103a62e36774412a040060f5574c7f08e19
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2023-02-02 11:06:11 -0500

    Added an image to the CardDeal component and finished the component

--- businessapp/src/components/carddeal/CardDeal.js
+++ businessapp/src/components/carddeal/CardDeal.js
@@ -5,7 +5,13 @@ import Button from "../button/Button"
 const CardDeal = () => (
   <section className={layout.section}>
     <div className={layout.sectionInfo}>
-      <h2>Find a better card deal <br className="sm:block hidden" /> in few easy steps.</h2>
+      <h2 className={styles.heading2}>Find a better card deal <br className="sm:block hidden" /> in few easy steps.</h2>
+      <p className={`${styles.paragraph} max-w-[480px] mt-5`}> Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
+        aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.</p>
+      <Button styles="mt-10" />
+    </div>
+    <div className={layout.sectionImg}>
+      <img className="w-[100%] h-[100%]" src={card} alt='card' />
     </div>
   </section>
 )
