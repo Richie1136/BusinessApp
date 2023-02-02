cdfc52764f90a45e6cdb9e3d32c8c1ea7002621a
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2023-02-02 11:44:30 -0500

    Building out the feedback card component

--- businessapp/src/components/feedbackcard/FeedbackCard.js
+++ businessapp/src/components/feedbackcard/FeedbackCard.js
@@ -1,8 +1,10 @@
+import { quotes } from "../../assets"
 
-const FeedbackCard = ({ name, title, image, content }) => {
-  return (
-    <div>FeedbackCard</div>
-  )
-}
+const FeedbackCard = ({ name, title, image, content }) => (
+  <div className="flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+    <img className="w-[42px] h-[27px] object-contain" src={quotes} alt='quotes' />
+    <p className="font-poppins font-normal text-[18px] leading-[32px] text-white my-10">{content}</p>
+  </div>
+)
 
 export default FeedbackCard
