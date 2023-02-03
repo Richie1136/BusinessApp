29e0a8f5f2e839b6e77b787dc478e8405ccf3775
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2023-02-03 09:54:18 -0500

    Starting to build out the ClickToAction component

--- businessapp/src/components/clicktoaction/ClickToAction.js
+++ businessapp/src/components/clicktoaction/ClickToAction.js
@@ -1,8 +1,16 @@
+import styles from "../../style"
+import Button from "../button/Button"
 
-const ClickToAction = () => {
-  return (
-    <div>ClickToAction</div>
-  )
-}
+const ClickToAction = () => (
+  <section>
+    <div>
+      <h2 className={styles.heading2}>Let's try our service now!</h2>
+      <p className={styles.paragraph}>Everything you need to accept card payments <br className="sm:block hidden" /> and grow your business anywhere on the planet.</p>
+    </div>
+    <div>
+      <Button />
+    </div>
+  </section>
+)
 
 export default ClickToAction
